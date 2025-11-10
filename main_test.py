88461edd4888e6f2cf8fbc788aab4e6884bb5e4b
lab.note.gota <yajima071224@gmail.com>
2025-11-10 15:38:31 +0900

    app起動スクリプトを本番用とテスト版に分離

--- main_test.py
+++ main_test.py
@@ -10,4 +10,7 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == "__main__":
+    src_path = "tests/data/TestExcel_org.xlsx"
+    path_excel = "TestExcel.xlsx"
+    shutil.copy(src_path, path_excel)
     main()
